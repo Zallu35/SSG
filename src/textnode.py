e71f0877c57ac149e98f70b464049f4a7a3f5c1f
Zallu35 <s14.sboss@gmail.com>
2025-10-03 02:39:00 -0400

    Made and tested function to convert text node to html node

--- src/textnode.py
+++ src/textnode.py
@@ -1,4 +1,5 @@
 from enum import Enum
+from htmlnode import HTMLNode, LeafNode, ParentNode
 
 class TextType(Enum):
     PLAIN = "Plain"
@@ -11,7 +12,7 @@ class TextType(Enum):
 class TextNode:
     def __init__(self, text, text_type, url=None):
         self.text = text
-        self.text_type = TextType(text_type)
+        self.text_type = text_type
         self.url = url
 
     def __eq__(self, other):
@@ -21,3 +22,18 @@ class TextNode:
     
     def __repr__(self):
         return f"TextNode({self.text}, {self.text_type.value}, {self.url})"
+
+def text_node_to_html_node(text_node):
+    tag_type = {
+        "Plain": None,
+        "Bold": "b",
+        "Italic": "i",
+        "Code": "code",
+        "Link": "a",
+        "Image": "img",
+    }
+    if text_node.text_type.value == "Link":
+        return LeafNode(tag_type["Link"], text_node.text, {"href": text_node.url})
+    if text_node.text_type.value == "Image":
+        return LeafNode(tag_type["Image"], "", {"src": text_node.url, "alt": text_node.text})
+    return LeafNode(tag_type[text_node.text_type.value], text_node.text)
